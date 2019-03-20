991375ec8244576afffecdd6a2958cd744316a9b
lucaszfolle <lucas.zfolle@gmail.com>
2019-03-20 11:23:13 -0300

    Added library files

---
+++ LoRaWAN.cpp
@@ -0,0 +1,322 @@
+/* ---------------------------------------------------
+        Radioenge Equipamentos de Telecomunicações
+   ---------------------------------------------------
+    This library contains a set of functions to configure
+    and operate the EndDevice LoRaWAN Radioenge
+  
+  Date: 19/03/19
+*/
+#include "LoRaWAN.h"
+#include <string.h>
+
+
+/* AT command strings */
+const char* str_at_commands[]
+{
+  "DADDR",
+  "APPKEY",
+  "APPSKEY",
+  "NWKSKEY",
+  "APPEUI",
+  "ADR",
+  "TXP",
+  "DR",
+  "DCS",
+  "PNM",
+  "RX2FQ",
+  "RX2DR",
+  "RX1DL",
+  "RX2DL",
+  "JN1DL",
+  "JN2DL",
+  "NJM",
+  "NWKID",
+  "CLASS",
+  "JOIN",
+  "NJS",
+  "SENDB",
+  "SEND",
+  "VER",
+  "CFM",
+  "SNR",
+  "RSSI",
+  "BAT",
+  "BAUDRATE",
+  "NBTRIALS",
+  "KEEPALIVE",
+  "TXCFM",
+  "CHMASK",
+  "ADC",
+  "GPIOC",
+  "WPIN",
+  "RPIN"
+};
+
+#define BUFFER_SIZE 256
+
+/* ----- Private Variables ----- */
+static SoftwareSerial* hSerialCommand = NULL;
+static SoftwareSerial* hSerialTransp = NULL;
+static uint8_t array[BUFFER_SIZE];
+static char g_payload[BUFFER_SIZE];
+
+/* ----- Public Variables ----- */
+
+/* ----- Private Functions ----- */
+
+void SerialFlush(SoftwareSerial* hSerial)
+{
+  while(hSerial->available() > 0)
+  {
+    hSerial->read();
+  }
+}
+
+
+
+/* ----- Public Function Definitions ----- */
+
+
+
+SoftwareSerial* SerialCommandsInit(uint8_t rxPin, uint8_t txPin, uint32_t baudRate)
+{
+  /* filter not used baudrates */
+  
+  static SoftwareSerial radioSerialCommands(rxPin, txPin);
+  radioSerialCommands.begin(baudRate);
+  hSerialCommand = &radioSerialCommands;
+
+  return &radioSerialCommands;
+}
+
+
+
+SoftwareSerial* SerialTranspInit(uint8_t rxPin, uint8_t txPin, uint32_t baudRate)
+{
+  /* filter not used baudrates */
+  
+  static SoftwareSerial radioSerialTransp(rxPin, txPin);
+  radioSerialTransp.begin(baudRate);
+  hSerialTransp = &radioSerialTransp;
+
+  return &radioSerialTransp;
+}
+
+
+
+
+Status_Typedef ReceivePacketCommand(char* payload, uint8_t* payloadSize, uint32_t timeout)
+{
+  uint16_t waitNextByte = 20;
+  uint8_t i = 0;
+  
+  /* Assert parameters */
+  if(payload == NULL) return RAD_ERROR;
+  if(payloadSize == NULL) return RAD_ERROR;
+  if(hSerialCommand == NULL) return RAD_ERROR;
+  
+  /* Waits for reception */
+  while( ((timeout > 0 ) || (i > 0)) && (waitNextByte > 0) )
+  {
+    if(hSerialCommand->available() > 0)
+    {
+      payload[i++] = hSerialCommand->read();
+	    waitNextByte = 20;
+    }
+    
+	  if(i > 0)
+    {
+      waitNextByte--;
+    }
+    timeout--;
+    delay(1);
+  }
+
+  /* In case it didn't get any data */
+  if((timeout == 0) && (i == 0)) return RAD_ERROR;
+
+  /* Copies payload size */
+  *payloadSize = i;
+  
+  return RAD_OK;
+}
+
+
+
+
+Status_Typedef ReceivePacketTransp(char* payload, uint8_t* payloadSize, uint32_t timeout)
+{
+  uint16_t waitNextByte = 20;
+  uint8_t i = 0;
+  
+  /* Assert parameters */
+  if(payload == NULL) return RAD_ERROR;
+  if(payloadSize == NULL) return RAD_ERROR;
+  if(hSerialTransp == NULL) return RAD_ERROR;
+  
+  /* Waits for reception */
+  while( ((timeout > 0 ) || (i > 0)) && (waitNextByte > 0) )
+  {
+    if(hSerialTransp->available() > 0)
+    {
+      payload[i++] = hSerialTransp->read();
+	    waitNextByte = 20;
+    }
+    
+	  if(i > 0)
+    {
+      waitNextByte--;
+    }
+    timeout--;
+    delay(1);
+  }
+
+  /* In case it didn't get any data */
+  if((timeout == 0) && (i == 0)) return RAD_ERROR;
+
+  /* Copies payload size */
+  *payloadSize = i;
+  
+  return RAD_OK;
+}
+
+
+
+Status_Typedef InitializeOTAA(char* p_appkey, char* p_appeui)
+{
+  char char_one[] = "1";
+  /* Checks the pointers */
+  if((p_appkey == NULL) || (p_appeui == NULL))
+  {
+    return RAD_ERROR;
+  }
+  /* checks the parameters */
+  if((strlen(p_appkey) != 47) || (strlen(p_appeui) != 23))
+  {
+    return RAD_ERROR;
+  }
+  
+  /* Configure Join mode to OTAA */
+  SendAtCommand(AT_NJM, AtSet, char_one);
+  delay(1000);
+  /* Configure APPKEY */
+  SendAtCommand(AT_APPKEY, AtSet, p_appkey);
+  delay(1000);
+  /* Configure APPEUI */
+  SendAtCommand(AT_APPEUI, AtSet, p_appeui);
+  delay(1000);
+}
+
+
+
+Status_Typedef IsJoined()
+{
+  char response[8];
+  uint8_t size;
+
+  SerialFlush(hSerialCommand);
+  SendAtCommand(AT_NJS, AtGet, NULL);
+  ReceivePacketCommand(response, &size, 500);
+
+  /* checks the response */
+  if(response[0] == '1')
+  {
+    return RAD_OK;
+  }
+  return RAD_ERROR;
+}
+
+
+
+Status_Typedef JoinNetwork(uint8_t retries)
+{
+  uint16_t remaining_retries = retries;
+
+  /* Checks if the EndDevice is already joined */
+  while( (IsJoined() != RAD_OK) && ((retries == 0) || (remaining_retries > 0)) )
+  {
+    delay(10000);
+    if(remaining_retries)
+    {
+      remaining_retries--;
+    }
+    SendAtCommand(AT_JOIN, AtRun, NULL);
+    delay(15000);
+  }
+  return IsJoined();
+}
+
+
+
+Status_Typedef SendString(char* string, uint8_t port)
+{
+  uint8_t size;
+
+  if(string == NULL)
+  {
+    return RAD_ERROR;
+  }
+  /* Checks the message length */
+  if(strnlen(string, BUFFER_SIZE) >= BUFFER_SIZE)
+  {
+    return RAD_ERROR;
+  }
+
+  /* Assemble the payload */
+  sprintf(g_payload, "%d:%s\0", port, string);
+
+  SendAtCommand(AT_SEND, AtSet, g_payload);
+}
+
+
+
+Status_Typedef SendAtCommand(AT_Commands_e command, CommandType_e command_type, char* payload)
+{
+  static const char at_string[] = "AT+";
+  static const char at_get_string[] = "=?";
+  static const char at_set_string[] = "=";
+  uint8_t index = 3;
+
+  if((payload == NULL) && (command_type == AtSet))
+  {
+    return RAD_ERROR;
+  }
+
+  /* Copies the AT+ prefix */
+  memset(array, 0, BUFFER_SIZE);
+  memcpy(array, at_string, 3);
+
+  /* Copies the command */
+  if(command >= NUMBER_OF_COMMANDS)
+  {
+    return RAD_ERROR;
+  }
+  memcpy(&array[index], str_at_commands[command], strlen(str_at_commands[command]));
+  index += strlen(str_at_commands[command]);
+
+  /* Copies the parameter */
+  if(command_type == AtGet)
+  {
+    memcpy(&array[index], at_get_string, 2);
+    index += 2;
+  }
+  else if(command_type == AtSet)
+  {
+    array[index] = at_set_string[0];
+    index += 1;
+    /* Appends the payload */
+    strcpy((char*)&array[index], payload);
+    index += strlen(payload);
+  }
+  else if(command_type != AtRun)
+  {
+    return RAD_ERROR;
+  }
+
+  array[index++] = '\r';
+  array[index++] = '\n';
+
+  /* Sends the message */
+  hSerialCommand->write(array, index);
+  delay(500);
+}
---
+++ LoRaWAN.h
@@ -0,0 +1,171 @@
+/* ---------------------------------------------------
+        Radioenge Equipamentos de Telecomunicações
+   ---------------------------------------------------
+    This library contains a set of functions to configure
+    and operate the EndDevice LoRaWAN Radioenge
+  
+  Date: 19/03/19
+*/
+#ifndef _LORA_WAN_
+#define _LORA_WAN_
+
+#include <stdint.h>
+#include "Arduino.h"
+#include <SoftwareSerial.h>
+
+/* ----- Defines ------ */
+
+typedef enum
+{
+  RAD_OK,
+  RAD_ERROR
+} Status_Typedef;
+
+
+typedef enum
+{
+  AtGet,
+  AtSet,
+  AtRun
+} CommandType_e;
+
+
+/* enum */
+typedef enum
+{
+  AT_DEVADDR,
+  AT_APPKEY,
+  AT_APPSKEY,
+  AT_NWKSKEY,
+  AT_APPEUI,
+  AT_ADR,
+  AT_TXP,
+  AT_DR,
+  AT_DCS,
+  AT_PNM,
+  AT_RX2FQ,
+  AT_RX2DR,
+  AT_RX1DL,
+  AT_RX2DL,
+  AT_JN1DL,
+  AT_JN2DL,
+  AT_NJM,
+  AT_NWKID,
+  AT_CLASS,
+  AT_JOIN,
+  AT_NJS,
+  AT_SENDB,
+  AT_SEND,
+  AT_VER,
+  AT_CFM,
+  AT_SNR,
+  AT_RSSI,
+  AT_BAT,
+  AT_BAUDRATE,
+  AT_NBTRIALS,
+  AT_KEEPALIVE,
+  AT_TXCFM,
+  AT_CHMASK,
+  AT_ADC,
+  AT_GPIOC,
+  AT_WPIN,
+  AT_RPIN,
+  NUMBER_OF_COMMANDS
+} AT_Commands_e;
+
+/* ----- Public global variables ----- */
+
+/* ----- Public Functions Prototype ----- */
+
+/**
+  * @brief Initializes Commands interface and the SoftwareSerial object on given Rx/Tx pins and Baudrate
+  * @param rxPin: Serial Rx pin
+  * @param txPin: Serial Tx pin
+  * @param baudRate: Serial baudrate, in bps
+  * @retval pointer to the SoftwareSerial object
+  */
+SoftwareSerial* SerialCommandsInit(uint8_t rxPin, uint8_t txPin, uint32_t baudRate);
+
+
+
+/**
+  * @brief Initializes Transparent interface and the SoftwareSerial object on given Rx/Tx pins and Baudrate
+  * @param rxPin: Serial Rx pin
+  * @param txPin: Serial Tx pin
+  * @param baudRate: Serial baudrate, in bps
+  * @retval pointer to the SoftwareSerial object
+  */
+SoftwareSerial* SerialTranspInit(uint8_t rxPin, uint8_t txPin, uint32_t baudRate);
+
+
+
+/**
+  * @brief Receives a packet from the commands interface
+  * @param payload[out]: buffer where the received payload will be copied to
+  * @param payloadSize[out]: received payload size
+  * @param timeout: reception timeout, in ms
+  * @retval RAD_OK or RAD_ERROR
+  */
+Status_Typedef ReceivePacketCommand(char* payload, uint8_t* payloadSize, uint32_t timeout);
+
+
+
+/**
+  * @brief Receives a packet from the transparent interface
+  * @param payload[out]: buffer where the received payload will be copied to
+  * @param payloadSize[out]: received payload size
+  * @param timeout: reception timeout, in ms
+  * @retval RAD_OK or RAD_ERROR
+  */
+Status_Typedef ReceivePacketTransp(char* payload, uint8_t* payloadSize, uint32_t timeout);
+
+
+
+/**
+  * @brief Initializes the EndDevice as OTAA with the given APPKEY and APPEUI
+  * @param p_appkey[in]: array containing the APPKEY as a string
+  * @param p_appeui[in]: array containing the APPEUI as a string
+  * @retval RAD_OK or RAD_ERROR
+  */
+Status_Typedef InitializeOTAA(char* p_appkey, char* p_appeui);
+
+
+
+/**
+  * @brief Checks if the EndDevice is joined on the network
+  * @retval RAD_OK or RAD_ERROR
+  */
+Status_Typedef IsJoined();
+
+
+
+/**
+  * @brief Sends a JOIN request to the network
+  * @param retries : number of retries to perform, 0 to infinite retries
+  * @retval RAD_OK or RAD_ERROR
+  */
+Status_Typedef JoinNetwork(uint8_t retries);
+
+
+
+/**
+  * @brief Sends a string to the specified port
+  * @param string : pointer to a string terminated with \0
+  * @param port : port number
+  * @retval RAD_OK or RAD_ERROR
+  */
+Status_Typedef SendString(char* string, uint8_t port);
+
+
+
+/**
+  * @brief Sends an AT command with the specified type and payload
+  * @param command : AT command of type AT_Commands_e
+  * @param command_type : AtGet, AtSet or AtRun
+  * @param payload : Payload terminated 
+  * @retval RAD_OK or RAD_ERROR
+  */
+Status_Typedef SendAtCommand(AT_Commands_e command, CommandType_e command_type, char* payload);
+
+
+#endif
