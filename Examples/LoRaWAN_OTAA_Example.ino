dd2279517de75cbf5484be3e9fd0b47319ebc59c
Gustavo Cereza <113127758+GustavoCereza@users.noreply.github.com>
2023-02-02 16:42:47 -0300

    Update LoRaWAN_OTAA_Example.ino

--- Examples/LoRaWAN_OTAA_Example.ino
+++ Examples/LoRaWAN_OTAA_Example.ino
@@ -1,19 +1,16 @@
 #include "LoRaWAN_Radioenge.h"
-#include "Stream.h"
 
 LoRaWAN_Radioenge LoRa(&Serial2);
-
-String AppKey = "50:D1:AD:39:E5:B3:A6:FC:FF:DB:3E:A6:3E:E6:5A:A4";
-String AppEui = "21:47:07:35:5E:D3:A3:A0";
 
 void setup() {
   Serial.begin(9600);
   Serial2.begin(9600);
+  
   LoRa.begin(true);
   LoRa.printParameters();
 
-  if(LoRa.JoinNetwork(OTAA, AppKey, AppEui, CS, true, true)) 
-    LoRa.SendString("teste", 1);
+  if(LoRa.JoinNetwork(OTAA, TTN, true, !true)) 
+    LoRa.SendString("elcereza.com", 1);
   
    LoRa.pinMode(2, OUTPUT);
 }
